182252df2090cc38fe81d0533f200aefb384780d
Smita Belan <SB00335952@techmahindra.com>
2020-09-06 12:19:16 +0530

    GetTotalPrice function added

--- Promotion_Engine/Program.cs
+++ Promotion_Engine/Program.cs
@@ -11,23 +11,43 @@ namespace Promotion_Engine
         static void Main(string[] args)
         {
         }
+
+        private static int GetTotalPrice(List<Product> Products)
+        {
+            int CounterA = 0;
+            int CounterB = 0;
+            int CounterC = 0;
+            int CounterD = 0;
+            int PriceA = 50;
+            int PriceB = 30;
+            int PriceC = 20;
+            int PriceD = 15;
+            foreach (Product pr in Products)
+            {
+                switch (pr.Id.ToUpper())
+                {
+                    case "A": CounterA++; break;
+                    case "B": CounterB++; break;
+                    case "C": CounterC++; break;
+                    case "D": CounterD++; break;
+                }
+                int ToatalPriceA = (CounterA / 3) * 130 + (CounterA % 3 * PriceA);
+                int ToatalPriceB = (CounterB / 2) * 45 + (CounterA % 2 * PriceB);
+                int ToatalPriceC = (CounterC * PriceC);
+                int ToatalPriceD = (CounterD * PriceD);
+                return ToatalPriceA + ToatalPriceB + ToatalPriceC + ToatalPriceD;
+            }
+        }
     }
 
     public  class Product
     {
         public string Id { get; set; }
-        public decimal Price { get; set; }
-        
+    
         public Product(string id)
         {
             this.Id = id;
-            switch(Id)
-            {
-                case "A": this.Price = 50m; break;
-                case "B": this.Price = 30m; break;
-                case "C": this.Price = 20m; break;
-                case "D": this.Price = 15m; break;
-            }
+           
         }
     }
 }
